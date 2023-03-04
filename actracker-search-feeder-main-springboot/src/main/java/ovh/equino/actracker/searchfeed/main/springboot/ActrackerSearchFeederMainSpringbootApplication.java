ab0c6a12a9d2696d531d382047cd53eb72c0aa7b
Marcin Ciapa <aaryndo@gmail.com>
2023-03-04 17:27:43 +0100

    Add maven wrapper, jenkinsfile, spring-boot project

---
+++ actracker-search-feeder-main-springboot/src/main/java/ovh/equino/actracker/searchfeed/main/springboot/ActrackerSearchFeederMainSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ovh.equino.actracker.searchfeed.main.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActrackerSearchFeederMainSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActrackerSearchFeederMainSpringbootApplication.class, args);
+	}
+
+}
