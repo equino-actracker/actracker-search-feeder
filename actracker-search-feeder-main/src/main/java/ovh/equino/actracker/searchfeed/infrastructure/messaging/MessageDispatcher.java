9c8d09efc18d48e96cb5b5becdf0fe6aa9a72682
Marcin Ciapa <aaryndo@gmail.com>
2023-03-30 23:37:28 +0200

    Get type from notification and use correct handler

--- actracker-search-feeder-main/src/main/java/ovh/equino/actracker/searchfeed/infrastructure/messaging/MessageDispatcher.java
+++ actracker-search-feeder-main/src/main/java/ovh/equino/actracker/searchfeed/infrastructure/messaging/MessageDispatcher.java
@@ -1,34 +1,45 @@
 package ovh.equino.actracker.searchfeed.infrastructure.messaging;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import ovh.equino.actracker.domain.Notification;
-import ovh.equino.actracker.domain.activity.ActivityChangedNotification;
 
+import java.util.List;
 import java.util.Set;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
-import static java.util.Collections.singleton;
+import static java.util.stream.Collectors.toSet;
 
 public class MessageDispatcher {
 
-    private final NotificationHandler<ActivityChangedNotification> activityHandler;
+    private final List<NotificationHandler<?>> notificationHandlers;
     private final ObjectMapper objectMapper;
 
-    MessageDispatcher(NotificationHandler<ActivityChangedNotification> activityHandler) {
-        this.activityHandler = activityHandler;
+    MessageDispatcher(List<NotificationHandler<?>> notificationHandlers) {
+        this.notificationHandlers = notificationHandlers;
         this.objectMapper = new ObjectMapper();
         this.objectMapper.registerModule(new JavaTimeModule());
         this.objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
+    public Set<Class<?>> supportedNotificationTypes() {
+        return notificationHandlers.stream()
+                .map(NotificationHandler::supportedNotificationType)
+                .collect(toSet());
+    }
+
     public void dispatchMessage(String rawMessage) {
         try {
-            Notification<?> notification =
-                    objectMapper.readValue(rawMessage, new TypeReference<Notification<ActivityChangedNotification>>() {});
-            activityHandler.handleNotification(notification);
+            Class<?> notificationType = getNotificationType(rawMessage);
+            JavaType javaType = objectMapper.getTypeFactory().constructParametricType(
+                    Notification.class,
+                    notificationType
+            );
+
+            Notification<?> notification = objectMapper.readValue(rawMessage, javaType);
+            handlerForType(notificationType).handleNotification(notification);
         } catch (JsonProcessingException e) {
             // TODO uncomment
 //            throw new RuntimeException(e);
@@ -36,7 +47,17 @@ public class MessageDispatcher {
         }
     }
 
-    public Set<Class<?>> supportedNotificationTypes() {
-        return singleton(activityHandler.supportedNotificationType());
+    private Class<?> getNotificationType(String rawMessage) throws JsonProcessingException {
+        Notification<?> notification = objectMapper.readValue(rawMessage, Notification.class);
+        return notification.notificationType();
+    }
+
+    private NotificationHandler<?> handlerForType(Class<?> notificationType) {
+        return notificationHandlers.stream()
+                .filter(handler -> handler.supportedNotificationType().equals(notificationType))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(
+                        "Handler not found for notification of type %s".formatted(notificationType.getCanonicalName())
+                ));
     }
 }
