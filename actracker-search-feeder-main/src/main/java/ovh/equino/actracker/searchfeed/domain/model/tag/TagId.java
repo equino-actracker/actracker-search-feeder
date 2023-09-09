46d98eef16067cae963fafbf7dd1c24e4efbf45a
Marcin Ciapa <aaryndo@gmail.com>
2023-09-09 13:48:04 +0200

    Add json property name to ID constructors

--- actracker-search-feeder-main/src/main/java/ovh/equino/actracker/searchfeed/domain/model/tag/TagId.java
+++ actracker-search-feeder-main/src/main/java/ovh/equino/actracker/searchfeed/domain/model/tag/TagId.java
@@ -1,12 +1,13 @@
 package ovh.equino.actracker.searchfeed.domain.model.tag;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ovh.equino.actracker.searchfeed.domain.model.EntityId;
 
 import java.util.UUID;
 
 public final class TagId extends EntityId {
 
-    public TagId(UUID id) {
+    public TagId(@JsonProperty("id") UUID id) {
         super(id);
     }
 }
