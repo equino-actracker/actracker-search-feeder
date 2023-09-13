71e9df951faec97c4d645cb75248802954480b25
Marcin Ciapa <aaryndo@gmail.com>
2023-09-13 21:01:35 +0200

    Delete in memory stores

--- actracker-search-feeder-main/src/main/java/ovh/equino/actracker/searchfeed/infrastructure/persistence/memory/InMemoryTagStore.java
+++
@@ -1,8 +0,0 @@
-package ovh.equino.actracker.searchfeed.infrastructure.persistence.memory;
-
-import ovh.equino.actracker.searchfeed.domain.model.tag.Tag;
-import ovh.equino.actracker.searchfeed.domain.model.tag.TagId;
-import ovh.equino.actracker.searchfeed.domain.model.tag.TagStore;
-
-final class InMemoryTagStore extends InMemoryEntityStore<TagId, Tag> implements TagStore {
-}
