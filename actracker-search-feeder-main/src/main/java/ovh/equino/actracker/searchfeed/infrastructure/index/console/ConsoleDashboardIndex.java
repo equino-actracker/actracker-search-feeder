9d466f3e33afcf833c26203c1e199f82e6dc403b
Marcin Ciapa <aaryndo@gmail.com>
2023-10-13 18:40:27 +0200

    Remove console indices in favor of Elasticsearch  indices

--- actracker-search-feeder-main/src/main/java/ovh/equino/actracker/searchfeed/infrastructure/index/console/ConsoleDashboardIndex.java
+++
@@ -1,8 +0,0 @@
-package ovh.equino.actracker.searchfeed.infrastructure.index.console;
-
-import ovh.equino.actracker.searchfeed.domain.model.dashboard.DashboardGraph;
-import ovh.equino.actracker.searchfeed.domain.model.dashboard.DashboardId;
-import ovh.equino.actracker.searchfeed.domain.model.dashboard.DashboardIndex;
-
-final class ConsoleDashboardIndex extends ConsoleEntityIndex<DashboardId, DashboardGraph> implements DashboardIndex {
-}
